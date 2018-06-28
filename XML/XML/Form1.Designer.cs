de84988d35dd2b25bcf8ab396febad07ae9e32d5
ShiFengZeng <90011551a90011551@yahoo.com.tw>
2018-06-28 22:03:12 +0800

    2018/06/28

---
+++ XML/XML/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace XML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.format_button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // format_button
+            // 
+            this.format_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.format_button.Location = new System.Drawing.Point(13, 729);
+            this.format_button.Name = "format_button";
+            this.format_button.Size = new System.Drawing.Size(128, 32);
+            this.format_button.TabIndex = 1;
+            this.format_button.Text = "format";
+            this.format_button.UseVisualStyleBackColor = true;
+            this.format_button.Click += new System.EventHandler(this.format_button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(907, 711);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 786);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.format_button);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XMLFormat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button format_button;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ XML/XML/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Web;
+namespace XML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.WordWrap = true;
+        }
+
+        private void format_button_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text.Trim().ToString();
+            //s = FilterXmlErrorCode(s);
+            s = PrettyXML(s);
+            textBox1.Clear();
+            textBox1.Text = s;
+        }        
+        private string PrettyXML(string XMLString)
+        {
+            StringWriter sw = new StringWriter();
+            XmlTextWriter xw = new XmlTextWriter(sw);
+            xw.Formatting = Formatting.Indented;
+            xw.Indentation = 4;
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(XMLString);
+            doc.Save(xw);
+            return sw.ToString();
+        }
+        
+    }
+}
